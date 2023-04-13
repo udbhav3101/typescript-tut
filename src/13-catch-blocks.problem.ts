4e9e3146d5a0a3752fdd7b4313dfefdcf824000b
udbhav_3101 <udbhav_govindu@bonify.de>
2023-04-13 23:16:04 +0530

    Solved e-13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -1,15 +1,45 @@
-import { expect, it } from "vitest";
+import {expect, it} from 'vitest';
 
-const tryCatchDemo = (state: "fail" | "succeed") => {
+// First solution
+
+// const tryCatchDemo = (state: "fail" | "succeed") => {
+//   try {
+//     if (state === "fail") {
+//       throw new Error("Failure!");
+//     }
+//   } catch (e: any) {
+//     return e.message;
+//   }
+// };
+
+// Second solution
+
+// const tryCatchDemo = (state: "fail" | "succeed") => {
+//   try {
+//     if (state === "fail") {
+//       throw new Error("Failure!");
+//     }
+//   } catch (e) {
+//     return (e as Error).message;
+//   }
+// };
+
+// Third solution
+
+const tryCatchDemo = (state: 'fail' | 'succeed') => {
   try {
-    if (state === "fail") {
-      throw new Error("Failure!");
+    if (state === 'fail') {
+      throw new Error('Failure!');
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof Error) return e.message;
   }
 };
 
-it("Should return the message when it fails", () => {
-  expect(tryCatchDemo("fail")).toEqual("Failure!");
+it('Should return the message when it fails', () => {
+  expect(tryCatchDemo('fail')).toEqual('Failure!');
+});
+
+it('Should return the message', () => {
+  expect(tryCatchDemo('succeed')).toEqual(undefined);
 });
